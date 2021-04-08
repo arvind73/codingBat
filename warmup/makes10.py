7cb2100299fde7817855c3bc73dec33bf7743b94
Arvind Rao <47332035+arvind73@users.noreply.github.com>
2021-04-08 09:54:19 +0530

    Update makes10.py

--- warmup/makes10.py
+++ warmup/makes10.py
@@ -11,3 +11,8 @@ def makes10(a, b):
   elif a+b==10:
     return True
   return False
+
+***********OR****************
+
+def makes10(a,b):
+  return (a+b == 10 or a==10 or b==10)  # Checks condition either True or False

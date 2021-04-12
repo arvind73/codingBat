48999f005d2464d25eec76318efbe5b4a34bf330
Arvind Rao <47332035+arvind73@users.noreply.github.com>
2021-04-12 23:00:47 +0530

    Create double_char.py

---
+++ strings/double_char.py
@@ -0,0 +1,12 @@
+Given a string, return a string where for every char in the original, there are two chars.
+
+
+double_char('The') → 'TThhee'
+double_char('AAbb') → 'AAAAbbbb'
+double_char('Hi-There') → 'HHii--TThheerree'
+
+def double_char(str):
+  new_str = ""
+  for char in str:
+    new_str += char*2
+  return new_str

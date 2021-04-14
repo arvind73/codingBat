74de54e624fa7a10416b2f5ef5dcee1870d928b7
Arvind Rao <47332035+arvind73@users.noreply.github.com>
2021-04-14 16:09:09 +0530

    Update string_times.py

--- Warmup-2/string_times.py
+++ Warmup-2/string_times.py
@@ -5,18 +5,19 @@ string_times('Hi', 2) → 'HiHi'
 string_times('Hi', 3) → 'HiHiHi'
 string_times('Hi', 1) → 'Hi'
  '''
-''' def string_times(str, n):
-      if n>0:
-    return n*str
-  else:
-    return ''
+''' 
+def string_times(str, n):
+    if n>0:
+        return n*str
+    else:
+        return ''
  '''
 # *********************OR**********************
 
 def string_times(str, n):
     result = ""
     for i in range(n):  # range(n) is [0, 1, 2, .... n-1]
-        result += str  # could use += here
+        result += str
     return result
 
 print(string_times('Hi', 4))
